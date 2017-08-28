633721ed9ed0e9c5492300383dce2bccf2f93387
Ryan Bucinell <rbucinell.dev@gmail.com>
2017-08-28 19:03:17 -0400

    trying to figure out these circminscribed circles

--- src/projects/gallifreyan/js/main.js
+++ src/projects/gallifreyan/js/main.js
@@ -67,14 +67,12 @@ vowels = {
     u: { height: 0, bar: -1}
 }
 
-
-
 var colors = ['#A93226', '#2471A3', '#229954', '#F4D03F', '#E67E22', '#34495E' ]
 
 var bourndyCircle ={
     x: center.x,
     y: center.y,
-    r: canvasHeight/2
+    r: canvasHeight/2*.7
 };
 var innerCircle = JSON.parse(JSON.stringify(bourndyCircle));
 innerCircle.r = bourndyCircle.r * .9;
@@ -114,6 +112,47 @@ function setup()
     angleMode(DEGREES);
 }
 
+function chord( angle, r)
+{
+    return 2*r*sin(angle/2);
+}
+
+function sagitta( radius, theta )
+{
+    return radius * ( 1 - Math.cos( DegToRad( theta / 2 ) ) )
+}
+
+/**
+ * Find the (x,y) cordinates of a given circle's radius and at an angle of theta
+ * 
+ * @param {Object} c the Circle
+ * @param {Number} theta angle of which to find the cords on
+ */
+function cordsOnCircle( c, theta )
+{
+    if( !c )
+    {
+        c= { x: 0,y:0};
+    }
+    return {
+        x: c.x + c.r * Math.cos( DegToRad(theta) ),
+        y: c.y + c.r * Math.sin( DegToRad(theta) )
+    }
+}
+
+/**
+ * 
+ * @param {Object} center Center Point (x,y)
+ * @param {Number} radius Radius distance
+ * @param {Number} theta the angle
+ */
+function cordsFromAngle( center, radius, theta)
+{
+    return cordsOnCircle({ x: center.x, y:center.y, r:radius }, theta);
+}
+
+
+
 //looping function.
 function draw()
 {
@@ -130,28 +169,92 @@ function draw()
     ellipse( center.x, center.y, 5);
     
 
-    words = ['one', 'two', 'three'];//, 'four', 'five', 'six', 'seven', 'eight'];
+    words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+
+    
     
     var theta = 360  / words.length;
-    var wordRadius = 2*innerCircle.r*sin(theta/2);
+    var wordRadius = chord( innerCircle.r, theta)/2;
+    var inscribeRadius = (innerCircle.r*2-wordRadius*2)/2;
 
-    stroke( 'red' );
-    ellipse( center.x, center.y, (innerCircle.r*2-wordRadius))
+    var p1 = cordsOnCircle( innerCircle, 0);
+    var p2 = cordsOnCircle( innerCircle, theta);
+    var mdpt ={ x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2 };
+    ellipse( mdpt.x, mdpt.y, 5);
+    line( center.x, center.y, mdpt.x, mdpt.y );
+    var chordRadius = Math.sqrt( Math.pow( mdpt.x-p1.x,2 ) + Math.pow( mdpt.y-p1.y,2 ) );
+    var wordCircle = JSON.parse(JSON.stringify(innerCircle));
+    wordCircle.r = chordRadius;
+    //stroke( 'red' );
+    //ellipse( center.x, center.y, inscribeRadius*2);
+
+    function getRadiusDistanceOfIntersectOfBiscectofTwoWedges(r, t1, t2 )
+    {
+        var p1 = cordsFromAngle( center.x, center.y, r, t1);
+        var p2 = cordsFromAngle( center.x, center.y, r, t2);
+        var dist = Math.sqrt( Math.pow( mdpt.x-p1.x,2 ) + Math.pow( mdpt.y-p1.y,2 ) );
+        return { x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2, d: dist };
+    }
 
     stroke( 'gray' );
     words.forEach(function(word,i) 
     {
-        //Set the 
+        var wordPt = getRadiusDistanceOfIntersectOfBiscectofTwoWedges( innerCircle.r, i*theta, (i+1)*theta );
+
+        var pt = cordsOnCircle( wordCircle, i*theta);
+        ellipse( pt.x, pt.y, wordCircle.r*2); //doesn't work
+
+
+        //var s = sagitta( innerCircle.r, i*theta);
+        //var dist = innerCircle.r - s;
+        //var Sx = dist * Math.cos( DegToRad(i*theta));
+        //var Sy = dist * Math.sin( DegToRad(i*theta));
+
+                //Set the 
         var curColor = colors.shift();
         stroke(curColor);
         colors.push(curColor);
+        
+        
+        var p1 = cordsOnCircle( innerCircle, gallifreyan_offset+ i*theta);
+        ellipse( p1.x, p1.y, chordRadius* 2);
+        ellipse( p1.x, p1.y, 5);
+
+        //if( i == 0)
+        //triangle( p1.x, p1.y, p2.x, p2.y, center.x, center.y);
 
 
-        var newX = center.x + (innerCircle.r) * Math.cos(DegToRad(gallifreyan_offset + i*theta));
-        var newY = center.y + (innerCircle.r) * Math.sin(DegToRad(gallifreyan_offset + i*theta));
-        ellipse( newX, newY, 5);
-        ellipse( newX, newY, wordRadius);
-        text( word, newX, newY);
+
+
+        
+
+        /*
+        var tempCords = cordsOnCircle( innerCircle, gallifreyan_offset+i*theta);
+        circleAnchor = {
+            index: i,
+            x: center.x + tempCords.x,
+            y: center.y + tempCords.y
+        };       
+
+
+        var inscribeCords = cordsOnCircle( inscribeRadius, gallifreyan_offset + i * theta );
+        inscribeAnchor = {
+            index: i+1,
+            x: center.x + inscribeCords.x,
+            y: center.y + inscribeCords.y
+        };
+        ellipse( inscribeAnchor.x, inscribeAnchor.y, 5);//inscribe point
+        text( 'i'+inscribeAnchor.index, inscribeAnchor.x, inscribeAnchor.y);//inscribe point text
+
+        ellipse( circleAnchor.x, circleAnchor.y, 5); //anchor on the innerCircle
+        text( word, circleAnchor.x, circleAnchor.y); //anchor on the innerCircle text
+
+        function d(r, theta )
+        {
+            return Math.sqrt( r*r - Math.pow( r * Math.sin( theta/2) ,2) );
+        }
+
+        ellipse( circleAnchor.x, circleAnchor.y, wordRadius*2); //draw the word ring*/
     
     });
     noLoop();
