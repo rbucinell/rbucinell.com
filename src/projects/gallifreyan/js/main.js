7af705adf5a4273994a8c5215fb647fa553ff370
Ryan Bucinell <rbucinell.dev@gmail.com>
2017-08-31 11:37:28 -0400

    redo of math to get incircle. added JSDocs

--- src/projects/gallifreyan/js/main.js
+++ src/projects/gallifreyan/js/main.js
@@ -6,6 +6,9 @@ var center = {
     x: canvasWidth/2,
     y: canvasHeight/2
 };
+var bourndyCircle ={ x: center.x, y: center.y, r: canvasHeight/2 * 0.7 };
+var innerCircle = JSON.parse(JSON.stringify(bourndyCircle));
+innerCircle.r = bourndyCircle.r * .9;
 
 LetterStyle = Object.freeze({
     TOUCH: 'touch',
@@ -20,6 +23,14 @@ AccentStyle = Object.freeze({
     BAR: 'bar'
 });
 
+vowels = {
+    a: { height: -1, bar: 0},
+    e: { height: 0, bar: 0},
+    i: { height: 0, bar: 1},
+    o: { height: 1, bar: 0},
+    u: { height: 0, bar: -1}
+}
+
 var consonants = {};
 function addLetter( letter, letterStyle, accentStyle, accentCount)
 {
@@ -28,7 +39,7 @@ function addLetter( letter, letterStyle, accentStyle, accentCount)
             type: accentStyle,
             count: accentCount
         },
-        style: letterStyle  //touch, hover, arc, bisec
+        style: letterStyle
     }
 }
 addLetter( 'b', LetterStyle.TOUCH, AccentStyle.BLANK, 0 );
@@ -59,28 +70,22 @@ addLetter( '&', LetterStyle.BISECT, AccentStyle.BAR, 3 );
 addLetter( '*', LetterStyle.BISECT, AccentStyle.BAR, 3 );
 addLetter( 'x', LetterStyle.BISECT, AccentStyle.BAR, 3 );
 
-vowels = {
-    a: { height: -1, bar: 0},
-    e: { height: 0, bar: 0},
-    i: { height: 0, bar: 1},
-    o: { height: 1, bar: 0},
-    u: { height: 0, bar: -1}
-}
+var colors = ['#A93226', '#2471A3', '#229954', '#F4D03F', '#E67E22', '#34495E' ];
 
-var colors = ['#A93226', '#2471A3', '#229954', '#F4D03F', '#E67E22', '#34495E' ]
 
-var bourndyCircle ={
-    x: center.x,
-    y: center.y,
-    r: canvasHeight/2*.7
-};
-var innerCircle = JSON.parse(JSON.stringify(bourndyCircle));
-innerCircle.r = bourndyCircle.r * .9;
 
 var words = [];
 
-function generateClick( e )
+/**
+ * Builds an array of words that have been entered and translated from the input field
+ * @returns {Array} an array of codded words to translate
+ */
+function getWords()
 {
+	if( document.getElementById("words").value === "")
+		{
+			document.getElementById("words").value = "one two three four five six seven eight";
+		}
     var text = document.getElementById("words").value.toLowerCase()
             .replace(' -', '-')
             .replace('- ', '-')
@@ -95,13 +100,26 @@ function generateClick( e )
     if( text.indexOf('c') !== -1 )
     {
         console.log( 'Error: Replace every C with K or an S');
+		words = [ 'error' ];
         return;
     }
+}
+
+/**
+ * onclick event handler for for generate button
+ * @event onclick 
+ * @param {event} e the event object
+ */
+function generateClick( e )
+{
+	getWords();
     loop();
 }
 
 
-//single run function that kicks off draw() loop
+/**
+ * The intial setup function of the p5js script. Will only execute once. 
+ */
 function setup()
 {
     canvas = createCanvas( canvasWidth, canvasHeight );
@@ -109,23 +127,62 @@ function setup()
     textSize( 32 );
     background(0,0,0,0);
     noLoop();
+	textSize( 20 );
     angleMode(DEGREES);
 }
 
+/**
+ * Converts an angle in degrees to radians
+ * @param {Number} deg The angle in degrees
+ */
+function DegToRad( deg )
+{
+    return (deg * Math.PI) / 180;
+}
+
+/**
+ * Converts an angle in radians to degrees
+ * @param {Number} rad The angle in radians
+ */
+function RadToDeg( rad )
+{
+    return (rad * 180) / Math.PI;
+}
+
+/**
+ * Gets the chord length between to lines
+ * @param {Number} angle The angle between the lines
+ * @param {Number} r The radius of the circle
+ */
 function chord( angle, r)
 {
     return 2*r*sin(angle/2);
 }
 
-function sagitta( radius, theta )
+/**
+ * Gets the distrance between two cordinate points
+ * @param {Object} p1 First cordinate point
+ * @param {Object} p2 Second cordinate point
+ */
+function ptDist( p1, p2 )
 {
-    return radius * ( 1 - Math.cos( DegToRad( theta / 2 ) ) )
+	return dist( p1.x, p1.y, p2.x, p2.y); //p5js library
+	//return Math.sqrt( Math.pow( p2.x-p1.x,2 ) + Math.pow(p2.y-p1.y,2));
 }
 
 /**
- * Find the (x,y) cordinates of a given circle's radius and at an angle of theta
- * 
- * @param {Object} c the Circle
+ * Gets the midpoint between two points
+ * @param {Object} p1 First cordinate point
+ * @param {Object} p2 Second cordinate point
+ */
+function midpoint( p1, p2 )
+{
+	return { x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2 };
+}
+
+/**
+ * Find the (x,y) cordinates of a given circle's radius and at an angle of theta * 
+ * @param {Object} c the Circle ({x,y,r} expected)
  * @param {Number} theta angle of which to find the cords on
  */
 function cordsOnCircle( c, theta )
@@ -141,19 +198,98 @@ function cordsOnCircle( c, theta )
 }
 
 /**
- * 
- * @param {Object} center Center Point (x,y)
- * @param {Number} radius Radius distance
- * @param {Number} theta the angle
+ * Finds the radius of an incircle of the trirangle ABC
+ * @param {Object} A The First vertex of the triangle
+ * @param {number} A.x The x componenent of the cordinate point
+ * @param {number} A.y The y componenent of the cordinate point
+ * @param {Object} B The Second vertex of the triangle
+ * @param {number} B.x The x componenent of the cordinate point
+ * @param {number} B.y The y componenent of the cordinate point
+ * @param {Object} C The Third vertex of the triangle
+ * @param {number} C.x The x componenent of the cordinate point
+ * @param {number} C.y The y componenent of the cordinate point
+ * @returns {number} the raidus of the incircle
  */
-function cordsFromAngle( center, radius, theta)
+function inCircleRadius( A, B, C )
 {
-    return cordsOnCircle({ x: center.x, y:center.y, r:radius }, theta);
+	var x = ptDist( A, B );
+	var y = ptDist( B, C );
+	var z = ptDist( C, A );
+	
+	return Math.sqrt( (x*y*z)/(x+y+z) );	
+}
+
+/**
+ * Finds the intersection point of two line segments p1p2 and p3p4 
+ * @param {Object} p1 The first Point of the first line segment
+ * @param {number} p1.x The x componenent of the cordinate point
+ * @param {number} p1.y The y componenent of the cordinate point
+ * @param {Object} p2 The second point of the first line segment
+ * @param {Object} p3 The first point of the second line  segment
+ * @param {Object} p4 The second point of the second line segment
+ * @returns {Object} Returns an object containg the location of where the lines would meet and if lies on either segment
+ */
+function intersectSegments( p1, p2, p3, p4)
+{
+	return line_intersect( p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y );
+}
+
+/**
+ * Finds the intersection point of two line segments p1p2 and p3p4 
+ * @param {number} x1 The first line segment's first point x component
+ * @param {number} y1 The first line segment's first point y component
+ * @param {number} x2 The first line segment's second point x component
+ * @param {number} y2 The first line segment's second point y component
+ * @param {number} x3 The second line segment's first point x component
+ * @param {number} y3 The second line segment's first point y component
+ * @param {number} x4 The second line segment's second point x component
+ * @param {number} y4 The second line segment's second point y component
+ * @returns {Object} Returns an object containg the location of where the lines would meet and if lies on either segment
+ */
+function line_intersect(x1, y1, x2, y2, x3, y3, x4, y4)
+{
+    var ua, ub, denom = (y4 - y3)*(x2 - x1) - (x4 - x3)*(y2 - y1);
+    if (denom == 0) {
+        return null;
+    }
+    ua = ((x4 - x3)*(y1 - y3) - (y4 - y3)*(x1 - x3))/denom;
+    ub = ((x2 - x1)*(y1 - y3) - (y2 - y1)*(x1 - x3))/denom;
+    return {
+        x: x1 + ua*(x2 - x1),
+        y: y1 + ua*(y2 - y1),
+        seg1: ua >= 0 && ua <= 1,
+        seg2: ub >= 0 && ub <= 1
+    };
 }
 
 
+/**
+ * Adds alpha to a color 
+ * @param {any} aColor A given color value
+ * @param {number} alpha Alpha 
+ * @returns {Color} a RGBA converted color
+ */
+function colorAlpha(aColor, alpha) {
+  var c = color(aColor);
+  return color('rgba(' +  [red(c), green(c), blue(c), alpha].join(',') + ')');
+}
 
-//looping function.
+/**
+ * A helper function to draw a dot and label at a given point using p5js functions 
+ * @param {Object} pt 
+ * @param {number} pt.x The x component of the pt
+ * @param {number} pt.y The y component of the pt
+ * @param {string} lbl The text of the label
+ */
+function ptLbl( pt, lbl )
+{
+	text( lbl, pt.x, pt.y );
+	ellipse( pt.x, pt.y, 5);
+}
+
+/**
+ * Main loop of the p5js script
+ */
 function draw()
 {
     clear();
@@ -161,111 +297,123 @@ function draw()
     //draw outer ring
     stroke( 'black' );
     ellipse(bourndyCircle.x,bourndyCircle.y, bourndyCircle.r*2);
-
     //draw inner ring
-    stroke( 'gray' );
+    stroke( '#555' );
     ellipse(innerCircle.x, innerCircle.y, innerCircle.r*2);
-    //center point
-    ellipse( center.x, center.y, 5);
     
-
-    words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-
-    
-    
+	//Determine the Max theta, no larger than 180
+	if( words.length === 0 )
+		getWords();
     var theta = 360  / words.length;
-    var wordRadius = chord( innerCircle.r, theta)/2;
-    var inscribeRadius = (innerCircle.r*2-wordRadius*2)/2;
+	
 
-    var p1 = cordsOnCircle( innerCircle, 0);
-    var p2 = cordsOnCircle( innerCircle, theta);
-    var mdpt ={ x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2 };
-    ellipse( mdpt.x, mdpt.y, 5);
-    line( center.x, center.y, mdpt.x, mdpt.y );
-    var chordRadius = Math.sqrt( Math.pow( mdpt.x-p1.x,2 ) + Math.pow( mdpt.y-p1.y,2 ) );
-    var wordCircle = JSON.parse(JSON.stringify(innerCircle));
-    wordCircle.r = chordRadius;
-    //stroke( 'red' );
-    //ellipse( center.x, center.y, inscribeRadius*2);
-
-    function getRadiusDistanceOfIntersectOfBiscectofTwoWedges(r, t1, t2 )
-    {
-        var p1 = cordsFromAngle( center.x, center.y, r, t1);
-        var p2 = cordsFromAngle( center.x, center.y, r, t2);
-        var dist = Math.sqrt( Math.pow( mdpt.x-p1.x,2 ) + Math.pow( mdpt.y-p1.y,2 ) );
-        return { x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2, d: dist };
-    }
-
-    stroke( 'gray' );
+	
+	var A = { x: center.x, y: center.y },
+		B =cordsOnCircle( innerCircle, 0 ),
+		C = cordsOnCircle( innerCircle, theta);
+	
+	ptLbl( A, 'A');
+	ptLbl( B, 'B');
+	ptLbl( C, 'C');
+	
+	//Midpoints of the line opposite the vector given vector
+	var mA = midpoint( B, C );
+	var mB = midpoint( A, C );
+	var mC = midpoint( A, B );
+	
+	ptLbl( mA, 'a' );
+	ptLbl( mB, 'b' );
+	ptLbl( mC, 'c' );
+	
+	var a = ptDist( B, C );
+	var b = ptDist( A, C );
+	var c = ptDist( A, B );
+	
+	//Draw ΔABC
+	stroke('black');
+	triangle( A.x, A.y, B.x, B.y, C.x, C.y );
+			
+	//Draw ΔTaTbTc
+	stroke('red');
+	triangle( mA.x, mA.y, mB.x, mB.y, mC.x, mC.y);
+	
+	//Draw bisect lines
+	stroke('#00DD00');
+	line( A.x, A.y, mA.x, mA.y );
+	line( B.x, B.y, mB.x, mB.y );
+	line( C.x, C.y, mC.x, mC.y );
+		
+	//Draw the inircle and the incenter	
+	stroke('blue');
+	fill(colorAlpha('blue', 0.15));
+	
+	var I = {
+		x: (a*A.x+b*B.x+c*C.x)/ (a+b+c),
+		y: (a*A.y+b*B.y+c*C.y)/ (a+b+c),
+		r: inCircleRadius( A, B, C)	
+	};
+	ptLbl( I, 'I');
+	
+	//Goal
+	ellipse( I.x, I.y, I.r );
+	
     words.forEach(function(word,i) 
     {
-        var wordPt = getRadiusDistanceOfIntersectOfBiscectofTwoWedges( innerCircle.r, i*theta, (i+1)*theta );
-
-        var pt = cordsOnCircle( wordCircle, i*theta);
-        ellipse( pt.x, pt.y, wordCircle.r*2); //doesn't work
-
-
-        //var s = sagitta( innerCircle.r, i*theta);
-        //var dist = innerCircle.r - s;
-        //var Sx = dist * Math.cos( DegToRad(i*theta));
-        //var Sy = dist * Math.sin( DegToRad(i*theta));
-
-                //Set the 
+        //Rotate the color
         var curColor = colors.shift();
         stroke(curColor);
         colors.push(curColor);
-        
-        
-        var p1 = cordsOnCircle( innerCircle, gallifreyan_offset+ i*theta);
-        ellipse( p1.x, p1.y, chordRadius* 2);
-        ellipse( p1.x, p1.y, 5);
+        noFill();
+		
+		
+        var A = { x: center.x, y: center.y },
+		B =cordsOnCircle( innerCircle, i*theta ),
+		C = cordsOnCircle( innerCircle, (i+1)*theta);
+	
+		ptLbl( A, 'A');
+		ptLbl( B, 'B');
+		ptLbl( C, 'C');
 
-        //if( i == 0)
-        //triangle( p1.x, p1.y, p2.x, p2.y, center.x, center.y);
+		//Midpoints of the line opposite the vector given vector
+		var mA = midpoint( B, C );
+		var mB = midpoint( A, C );
+		var mC = midpoint( A, B );
 
+		ptLbl( mA, 'a' );
+		ptLbl( mB, 'b' );
+		ptLbl( mC, 'c' );
 
+		var a = ptDist( B, C );
+		var b = ptDist( A, C );
+		var c = ptDist( A, B );
 
+		//Draw ΔABC
+		stroke('black');
+		triangle( A.x, A.y, B.x, B.y, C.x, C.y );
 
-        
+		//Draw ΔTaTbTc
+		stroke('red');
+		triangle( mA.x, mA.y, mB.x, mB.y, mC.x, mC.y);
 
-        /*
-        var tempCords = cordsOnCircle( innerCircle, gallifreyan_offset+i*theta);
-        circleAnchor = {
-            index: i,
-            x: center.x + tempCords.x,
-            y: center.y + tempCords.y
-        };       
+		//Draw bisect lines
+		stroke('#00DD00');
+		line( A.x, A.y, mA.x, mA.y );
+		line( B.x, B.y, mB.x, mB.y );
+		line( C.x, C.y, mC.x, mC.y );
 
+		//Draw the inircle and the incenter	
+		stroke('blue');
+		fill(colorAlpha('blue', 0.15));
 
-        var inscribeCords = cordsOnCircle( inscribeRadius, gallifreyan_offset + i * theta );
-        inscribeAnchor = {
-            index: i+1,
-            x: center.x + inscribeCords.x,
-            y: center.y + inscribeCords.y
-        };
-        ellipse( inscribeAnchor.x, inscribeAnchor.y, 5);//inscribe point
-        text( 'i'+inscribeAnchor.index, inscribeAnchor.x, inscribeAnchor.y);//inscribe point text
+		var I = {
+			x: (a*A.x+b*B.x+c*C.x)/ (a+b+c),
+			y: (a*A.y+b*B.y+c*C.y)/ (a+b+c),
+			r: inCircleRadius( A, B, C)	
+		};
+		ptLbl( I, 'I');
 
-        ellipse( circleAnchor.x, circleAnchor.y, 5); //anchor on the innerCircle
-        text( word, circleAnchor.x, circleAnchor.y); //anchor on the innerCircle text
-
-        function d(r, theta )
-        {
-            return Math.sqrt( r*r - Math.pow( r * Math.sin( theta/2) ,2) );
-        }
-
-        ellipse( circleAnchor.x, circleAnchor.y, wordRadius*2); //draw the word ring*/
-    
+		//Goal
+		ellipse( B.x, B.r, I.r );
     });
     noLoop();
-}
-
-function DegToRad( deg )
-{
-    return (deg * Math.PI) / 180;
-}
-
-function RadToDeg( rad )
-{
-    return (rad * 180) / Math.PI;
 }
