dde88310d86457cbd1d15456b563cae1436c130b
Bucinell, Ryan M <rbucinell.dev@gmail.com>
2019-07-31 15:15:18 -0400

    switched dragging support to toggle

--- src/projects/life/js/Grid.js
+++ src/projects/life/js/Grid.js
@@ -43,14 +43,17 @@ class Grid
 		return point.x >= this.x && point.x < this.x + this.width && point.y >= this.y && point.y < this.y + this.height;
 	}
 
-	click( point )
+	click( point, previousSqDragged = null )
 	{		
 		//This is a quick and dirty way to grab what square was clicked on. May need to consider checking more squares if this fails
         const   apprxSqX = parseInt((point.x- this.x) / SQUARE_SIZE), 
                 apprxSqY = parseInt((point.y- this.y) / SQUARE_SIZE );
-        const sqr = grid.squares[apprxSqY][apprxSqX];
-        if( sqr.hitTest( point ) )
-			sqr.toggleState();
+		const sqr = grid.squares[apprxSqY][apprxSqX];
+		
+		if( previousSqDragged === null || previousSqDragged !== sqr )
+			if( sqr.hitTest( point ) )
+				sqr.toggleState();
+		return sqr;
 	}
 
 	draw()
--- src/projects/life/js/sketch.js
+++ src/projects/life/js/sketch.js
@@ -1,40 +1,19 @@
-/*
-var canvas = document.getElementById("canvas");
-var graphics = canvas.getContext("2d");
-var game = new Game(canvas.width, canvas.height, graphics);
 
-if (document.loaded) {
-    DoStuffFunction();
-} else {
-    if (window.addEventListener) {
-        window.addEventListener('load', DoStuffFunction, false);
-    } else {
-        window.attachEvent('onload', DoStuffFunction);
-    }
-}
-
-function DoStuffFunction() {
-    // add our events. Up and down are for dragging,
-    // double click is for making new boxes
-    canvas.onmousedown = gameMouseDown;
-    canvas.onmouseup = gameMouseUp;
-    canvas.ondblclick = gameMouseDoubleClick;
-}
-*/
 let grid;
 
 function setup()
 {
-    grid = new Grid(0,0, 800, 400, 40, 20, 'config/stills.json' );
+    grid = new Grid(0,0, 800, 400, 40, 20, 'config/clear.json' );
     cnvs = createCanvas( 800, 400 );
 }
 
-function mouseClicked()
+let previousSqDragged;
+function mouseDragged()
 {
     const pt = { x: mouseX, y: mouseY };
     if( grid.hitTest( pt ) )
     {
-        grid.click( pt );
+        previousSqDragged = grid.click( pt,previousSqDragged );
     }
     //try to 
 }
