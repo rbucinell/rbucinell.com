9786714806bec7f17b985ad49a0245c22b5ae8f6
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-09-22 01:27:30 -0400

    using file format to fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  }
   //TODO: After tech update, remove bootstrap and re-enable tailwind
   //integrations: [tailwind()]
 });
